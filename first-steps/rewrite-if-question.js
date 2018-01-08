68ee299595407dc48950665768e48e99d919b68c
Pavel Karachev <karachevpavel@gmail.com>
2018-01-08 21:40:49 +0300

    Решение задания - Перепишите 'if' в '?'

---
+++ first-steps/rewrite-if-question.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+Перепишите if с использованием оператора '?':
+
+if (a + b < 4) {
+    result = 'Мало';
+} else {
+    result = 'Много';
+}*/
+
+let a = 1;
+let b = 1;
+
+let result = (a + b < 4) ? "Мало" : "Много";
+console.log(result);
