887685f1260713e9dd31b861567b43fed6e2ac03
Pavel Karachev <pavelkarachev@ymail.com>
2018-01-11 22:19:08 +0300

    Добавляет решение - Случайное из интервала (0, max)

---
+++ first-steps/random-0-max.js
@@ -0,0 +1,8 @@
+// Напишите код для генерации случайного значения в диапазоне от 0 до max, не включая max.
+
+function getRandomArray(max) {
+
+  return Math.random() * max;
+}
+
+console.log(getRandomArray(10));
