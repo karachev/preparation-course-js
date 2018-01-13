56536adc084e6b80fd3ba21ada715c2d045da0e3
Pavel Karachev <karachevpavel@gmail.com>
2018-01-13 17:42:48 +0300

    Добавляет решение - Сделать первый символ заглавным

---
+++ data-structure/ucfirst.js
@@ -0,0 +1,12 @@
+// Напишите функцию ucFirst(str), которая возвращает строку str с заглавным первым символом, например:
+//
+// ucFirst("вася") == "Вася";
+// ucFirst("") == ""; // нет ошибок при пустой строке
+// P.S. В JavaScript нет встроенного метода для этого. Создайте функцию, используя toUpperCase() и charAt().
+
+function ucFirst(str) {
+  let newStr = str.charAt(0).toUpperCase() + str.slice(1);
+  return newStr;
+}
+
+console.log(ucFirst("вася"));
