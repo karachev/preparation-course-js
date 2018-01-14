4130d59885c5f105a6979bbf347355d3003f3b65
Pavel Karachev <karachevpavel@gmail.com>
2018-01-14 14:09:29 +0300

    Добавляет решение - Умножьте численные свойства на 2

---
+++ data-structure/multiply-numeric.js
@@ -0,0 +1,22 @@
+// Создайте функцию multiplyNumeric , которая получает объект и умножает все численные свойства на 2. Например:
+
+var menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+console.log(multiplyNumeric(menu));
+
+function multiplyNumeric(menu) {
+  for (let key in menu) {
+    if (isNumeric(menu[key])) {
+      menu[key] *= 2;
+    }
+  }
+  return menu;
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n)
+}
