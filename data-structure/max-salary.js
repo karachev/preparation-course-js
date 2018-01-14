0633c4b334644f4d3f1d3cd66e1ff2326e7c3684
Pavel Karachev <karachevpavel@gmail.com>
2018-01-14 13:57:54 +0300

    Добавляет решение - Свойство с наибольшим значением

---
+++ data-structure/max-salary.js
@@ -0,0 +1,23 @@
+/*Есть объект salaries с зарплатами. Напишите код, который выведет имя сотрудника, у которого самая большая зарплата.
+Если объект пустой, то пусть он выводит «нет сотрудников».*/
+
+"use strict";
+var salaries = {
+  "Вася": 100,
+  "Петя": 300,
+  "Даша": 250
+};
+
+console.log(showSalaries(salaries));
+
+function showSalaries(salaries) {
+  let maxSalaries = 0;
+  let name = "";
+  for (let maxName in salaries) {
+    if (salaries[maxName] > maxSalaries) {
+      maxSalaries = salaries[maxName];
+      name = maxName;
+    }
+  }
+  return name || "нет сотрудников";
+}
