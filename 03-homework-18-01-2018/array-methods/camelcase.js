0bb69a24725de8419a26748035c3d9d25195c0ac
Pavel Karachev <pavelkarachev@ymail.com>
2018-01-16 22:18:19 +0300

    Добавляет решение - Перевести текст вида border-left-width в borderLeftWidth

--- 03-homework-18-01-2018/array-methods/camelcase.js
+++ 03-homework-18-01-2018/array-methods/camelcase.js
@@ -1,30 +1,21 @@
 /*
 Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
-
 То есть, дефисы удаляются, а все слова после них получают заглавную букву.
-
 Например:
-
 camelize("background-color") == 'backgroundColor';
 camelize("list-style-image") == 'listStyleImage';
 camelize("-webkit-transition") == 'WebkitTransition';
+
 Такая функция полезна при работе с CSS.
+P.S. Вам пригодятся методы строк charAt, split и toUpperCase.
+*/
 
-  P.S. Вам пригодятся методы строк charAt, split и toUpperCase.*/
-
-function camelize(string) {
-  let array = string.split();
-  console.log(array);
-  // for (let i = 0; i < string.length; i++) {
-  //   array.push(string[i]);
-  // }
-  // for (let i = 0; i < array.length; i++) {
-  //   if (array[i] = "-") {
-  //     array[i+1] = array[i+1].toUpperCase();
-  //   }
-  //
-  // }
-  // return string;
+function camelize(str) {
+  let array = str.split('-');
+  for (let i = 1; i < array.length; i++) {
+    array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+  }
+  return array.join('');
 }
 
 console.log(camelize("background-color")); // == 'backgroundColor';
