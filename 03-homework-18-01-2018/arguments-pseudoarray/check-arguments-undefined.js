4deac9e7799f7a46928f2ca6b2049ea558fb3373
Pavel Karachev <karachevpavel@gmail.com>
2018-01-18 11:53:07 +0300

    Добавляет решение - Проверка на аргумент-undefined

---
+++ 03-homework-18-01-2018/arguments-pseudoarray/check-arguments-undefined.js
@@ -0,0 +1,19 @@
+/*
+Как в функции отличить отсутствующий аргумент от undefined?
+
+function f(x) {
+  // ..ваш код..
+  // выведите 1, если первый аргумент есть, и 0 - если нет
+}
+
+f(undefined); // 1
+f(); // 0*/
+
+
+console.log(f(undefined));; // 1
+console.log(f());; // 0*/
+
+
+function f(x) {
+  return (arguments.length) ? 1 : 0;
+}
