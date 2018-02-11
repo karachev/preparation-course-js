5e55764ee9e4c7ab005315ac33e38540900bd23e
Pavel Karachev <karachevpavel@gmail.com>
2018-02-11 13:39:25 +0300

    Добавляет решение Сортировка

---
+++ js/07-homework-05-02-2018/closures-usage/sort-by-field.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let users = [{
+  name: "Вася",
+  surname: 'Иванов',
+  age: 20
+}, {
+  name: "Петя",
+  surname: 'Чапаев',
+  age: 25
+}, {
+  name: "Маша",
+  surname: 'Медведева',
+  age: 18
+}];
+
+users.sort(byField('name'));
+users.forEach(function(user) {
+  console.log( user.name );
+}); // Вася, Маша, Петя
+
+users.sort(byField('age'));
+users.forEach(function(user) {
+  console.log( user.name );
+}); // Маша, Вася, Петя
+
+function byField(argument) {
+  return function (a, b) {
+    return a[argument] > b[argument] ? 1 : -1;
+  };
+}
