288aff4cef0aff7384aeb783731d9e3a355d52f1
Pavel Karachev <pavelkarachev@ymail.com>
2018-02-20 21:40:02 +0300

    Добавляет решение - Алгоритм для поиска

---
+++ js/10-homework-15-02-2018/prototype/search-algorithm.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let head = {
+  glasses: 1
+};
+
+let table = {
+  pen: 3
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2
+};
+
+let pockets = {
+  money: 2000
+};
+
+pockets.__proto__ = bed;
+bed.__proto__ = table;
+table.__proto__ = head;
+
+console.log(pockets.pen); //== 3;
+console.log(bed.glasses); //== 1;
+console.log(table.money); //== undefined;
